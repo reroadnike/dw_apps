402381dc29d3be9ff63f2a7f8d4f1861a9fb96d7
reroadnike <1531900560@qq.com>
2019-08-22 14:05:45 +0800

    配置域名

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        echo appps;
+        $data = M('Admin')->select();
+        $this->ajaxReturn($data);
     }
 }
